0c7b3ea88d14cf1db38c3c9cf09decd9651df56e
xloem <0xloem@gmail.com>
2021-03-20 12:52:23 -0400

    wip

--- libonchain/data.hpp
+++ libonchain/data.hpp
@@ -20,24 +20,25 @@ public:
     std::string const name;
     std::string const technology;
     std::string const address;
-    std::vector<std::string> const keys;
+    std::string const key;
     std::vector<std::string> const values;
     std::unordered_set<Flag> const flags;
 
     // add more features/etc as use is discovered
-    virtual void add(std::string const & key, std::vector<uint8_t> const & value);
-    virtual std::vector<uint8_t> get(std::string const & key);
+    virtual void add(std::string const & key, std::vector<std::vector<uint8_t>> const & values);
+    virtual std::vector<std::vector<uint8_t>> get(std::string const & key);
     virtual void drop(std::string const & key);
 
-    inline virtual_iterator<char const *> begin() { return begin_new(); }
-    inline virtual_iterator<char const *> end() { return end_new(); }
+    inline virtual_iterator<char const *> begin() { return new_begin(); }
+    inline virtual_iterator<char const *> end() { return new_end(); }
 
 protected:
-    Data(std::string const & technology, std::string const & address, std::vector<std::string> const & keys, std::vector<std::string> const & values, std::vector<Flag> const & flags);
+    Data(std::string const & technology, std::string const & address, std::string const & key, std::vector<std::string> const & values, std::vector<Flag> const & flags);
     ~Data();
 
-    virtual virtual_iterator<char const *>::impl * begin_new() = 0;
-    virtual virtual_iterator<char const *>::impl * end_new() = 0;
+    virtual virtual_iterator<char const *>::impl * new_begin() = 0;
+    virtual virtual_iterator<char const *>::impl * new_end() = 0;
+    //virtual virtual_iterator<std::vector<std::vector<uint8_t>>>::impl * get(std::map<std::string> const & keys);
 };
 
 } // namespace libonchain
